0bbe303e0837f31bf096e1e8e34f445cf3500f24
Obakeng Dusani <obakeng.dusani@bothouniversity.ac.bw>
2022-07-24 17:47:21 +0200

    Added arrow function to Greet component

--- app/src/components/Greet.js
+++ app/src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function Greet(){
-    return <h4>Hello Obakeng</h4>
-}
+// function Greet(){
+//     return <h4>Hello Obakeng</h4>
+// }
+
+const Greet = () => <h3>Hello Dusani</h3>
 
 export default Greet;
